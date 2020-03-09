0f0060dda9d6f1d6d24591de20c2531732dfdca8
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2020-03-09 18:03:15 -0400

    Initial commit - Form Handling

---
+++ src/main/java/com/jwd/springboot_201a/Springboot201aApplication.java
@@ -0,0 +1,13 @@
+package com.jwd.springboot_201a;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot201aApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springboot201aApplication.class, args);
+    }
+
+}
